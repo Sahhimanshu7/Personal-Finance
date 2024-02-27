d67d0136edd2cc8ff2fcdb5100f845041ba7e8de
Himanshu Sah <himanshusah@Himanshus-Air.lan>
2024-02-27 12:10:34 -0500

    Server

---
+++ server/config/mongo.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongo connection started successfully!");
+});
+mongoose.connection.on("reconnected", () => {
+  console.log("Mongo connection reestablished.");
+});
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongo connection is disconnected.");
+})
---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import "./config/mongo.js";
+
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8080;
+
+// Implement routes
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
